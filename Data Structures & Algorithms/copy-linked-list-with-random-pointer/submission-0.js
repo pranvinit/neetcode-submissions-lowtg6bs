53cefdb41257e7494bcb25a5e50adeebe646711f
Pranav Yeole <83200717+pranvinit@users.noreply.github.com>
2026-04-07 18:44:21 +0530

    Add: copy-linked-list-with-random-pointer - submission-0

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-0.js
@@ -0,0 +1,35 @@
+// class Node {
+//   constructor(val, next = null, random = null) {
+//       this.val = val;
+//       this.next = next;
+//       this.random = random;
+//   }
+// }
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+    copyRandomList(head) {
+        if (!head) return null;
+        let curr = head;
+        let map = new Map();
+
+        while(curr){
+            let copy = new Node(curr.val)
+            map.set(curr, copy);
+            curr = curr.next;
+        }
+
+        curr = head;
+        while(curr){
+            let copy = map.get(curr);
+            copy.next = map.get(curr.next) || null;
+            copy.random = map.get(curr.random) || null;
+            curr = curr.next;
+        }
+
+        return map.get(head);
+    }
+}
